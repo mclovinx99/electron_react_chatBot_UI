874d54bee779fe072d97c8402624d2cf7859b4b5
Mclovinx99 <aditya.rathi.hestabit@gmail.com>
2024-04-12 19:55:27 +0530

    updated UI

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -20,25 +20,47 @@ const Home = () => {
 					<div className="main-content">
 						<div className='main-heading'>
 							<div className='chat-sec'>
-								<h1 className="mt-3">Welcome to Ai Interview Assistant</h1>
-								<span className='p-3'>
-									<h5>Maximizing Interview Success Through AI-Powered Guidance</h5>
-								</span>
-								<div class="info-container">
-									<div class="info">
-										<img src={elipse} style={{ width: '5%' }} />
-										<p> <b>Accurate Answers</b></p>
-										<p>Trustworthy, precise responses tailored to you.</p>
+								{/* <div className='chat-heading'>
+									<h1 className="mt-3">Welcome to Ai Interview Assistant</h1>
+									<span className='p-3'>
+										<h5>Maximizing Interview Success Through AI-Powered Guidance</h5>
+									</span>
+									<div class="info-container">
+										<div class="info">
+											<img src={elipse} style={{ width: '5%' }} />
+											<p> <b>Accurate Answers</b></p>
+											<p>Trustworthy, precise responses tailored to you.</p>
+										</div>
+										<div class="info">
+											<img src={elipse} style={{ width: '5%' }} />
+											<p><b>Audio Analysis</b></p>
+											<p>Deep insights from cutting-edge audio technology.</p>
+										</div>
+										<div class="info">
+											<img src={elipse} style={{ width: '5%' }} />
+											<p><b>Fast response</b></p>
+											<p>Rapid assistance for seamless productivity.</p>
+										</div>
 									</div>
-									<div class="info">
-										<img src={elipse} style={{ width: '5%' }} />
-										<p><b>Audio Analysis</b></p>
-										<p>Deep insights from cutting-edge audio technology.</p>
+								</div> */}
+								<div className='chat-box'>
+									{/* Questions */}
+									<div>
+										<img className="rounded p-1" src={recordIcon} style={{ backgroundColor: 'white', margin: '6px' }} />
+										<div class="d-flex flex-column flex-grow-1 p-1">
+											<div class="flex-grow-1">
+												In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.
+											</div>
+										</div>
 									</div>
-									<div class="info">
-										<img src={elipse} style={{ width: '5%' }} />
-										<p><b>Fast response</b></p>
-										<p>Rapid assistance for seamless productivity.</p>
+									{/* Response */}
+									<div>
+										<img className="rounded p-1" src={recordIcon} style={{ backgroundColor: 'white', margin: '6px' }} />
+										<div class="d-flex flex-column flex-grow-1 p-1">
+											<div class="flex-grow-1">
+												In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -50,7 +72,7 @@ const Home = () => {
 									<div className="input-group d-flex">
 										<input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Type your question" />
 										<button type="submit" className="btn btn-primary">
-											<img src={SendIcon} style={{ width: '4vh' }} />
+											<img src={SendIcon} style={{ width: '30px' }} />
 										</button>
 									</div>
 								</form>
